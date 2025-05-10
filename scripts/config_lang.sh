685419f9de1b6bb5d94a0e1d38de25a6bf07ee70
tkclme <clement.lrd@hotmail.fr>
2025-05-10 14:31:46 +0200

    Update config_lang.sh

--- scripts/config_lang.sh
+++ scripts/config_lang.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-
 # Configuration de langue
+echo "=== Configuring language ==="
 
-sudo locale-gen fr_FR.UTF-8
+sudo locale-gen fr_FR.UTF-
 sudo update-locale LANG=fr_FR.UTF-8
+
 export LANG=fr_FR.UTF-8
 
-
 # Configuration de la disposition clavier
+echo "=== Configuring keyboard layout ==="
 
-sudo sed -i 's/XKBLAYOUT=.*/XKBLAYOUT="fr"/' /etc/default/keyboard
-sudo setupcon
-sudo udevadm trigger --subsystem-match=input --action=change
-
+if [ -f /etc/default/keyboard ]; then
+    sudo sed -i 's/XKBLAYOUT=.*/XKBLAYOUT="fr"/' /etc/default/keyboard
+    sudo setupcon
+    sudo udevadm trigger --subsystem-match=input --action=change
+else
+    echo "Erreur : fichier /etc/default/keyboard introuvable."
+fi
